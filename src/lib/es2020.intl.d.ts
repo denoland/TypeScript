36b704b0b66dc4f2fec50dbe67f4b07dd7bc034c
David Sherret <dsherret@gmail.com>
2024-01-05 09:51:58 -0500

    Update for latest temporal changes (might be reverted)

--- src/lib/es2020.intl.d.ts
+++ src/lib/es2020.intl.d.ts
@@ -249,7 +249,7 @@ declare namespace Intl {
     }
 
     interface DateTimeFormatOptions {
-        calendar?: string | undefined;
+        calendar?: string | (typeof globalThis extends { Temporal: { CalendarProtocol: infer T; }; } ? T : undefined) | undefined;
         dayPeriod?: "narrow" | "short" | "long" | undefined;
         numberingSystem?: string | undefined;
 
